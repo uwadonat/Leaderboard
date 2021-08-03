ea66c565a729ce8fe4a7863349b59b7831738d3b
uwadonat <uwadonat@gmail.com>
2021-08-03 15:28:23 +0200

    Add index.js file

--- src/leaderboardlayout.js
+++ src/leaderboardlayout.js
@@ -1,3 +1,8 @@
 export default function leaderboardlayout() {
-    
+    const scores = [{ name: 'Donat', score: 20}, {name:'Evodie', score:70}, {name: 'Paradis',score:50}, {name:'Rosine', score:15}];
+    const container = document.createElement('div');
+    container.className = 'container';
+    const leaderboardTitle = document.createElement('h1');
+    leaderboardTitle.textContent = 'Leaderboard';
+    container.appendChild(leaderboardTitle);
 }
